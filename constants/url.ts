ceb601c5a378d099786b1cefa9431d3d6fdb4d95
ansar <mohammedansarusman@gmail.com>
2025-12-07 11:41:44 +0400

    deployment

--- constants/url.ts
+++ constants/url.ts
@@ -1,2 +1,2 @@
-export const PRODUCTS_URL = "https://fakestoreapi.com/products/"
+export const PRODUCTS_URL = "https://fakestoreapi.com/products"
 export const CATEGORIES_URL ="https://fakestoreapi.com/products/categories"
