b550dba50df68f0c99d002d43073c3ae572d12ea
ansar <mohammedansarusman@gmail.com>
2025-12-08 22:33:55 +0400

    amend

--- requests/request.ts
+++ requests/request.ts
@@ -8,11 +8,23 @@ export const fetchCategory = async()=>{
 }
 
 
-export const fetchProducts = async() =>{
-    const response = await fetch(PRODUCTS_URL,{next: { revalidate: 10 }})
-    const data = await response.json();
-    return data;
-}
+export const fetchProducts = async () => {
+  const res = await fetch(PRODUCTS_URL, {
+    cache: "no-store"
+  });
+
+  if (!res.ok) {
+    console.error("API error", res.status);
+    return [];
+  }
+
+  try {
+    return await res.json();
+  } catch {
+    return [];
+  }
+};
+
 export const singleProduct = async (id: string) => {
   const response = await fetch(`https://fakestoreapi.com/products/${id}`, {
     cache: "no-store",
